ab7e9f9f97039db1465a5cfa6482c1922495149c
Gerardo Marset <gammer1994@gmail.com>
2022-05-19 15:15:51 -0300

    Initial commit

---
+++ HitboxView/HitboxModule.cs
@@ -0,0 +1,154 @@
+﻿using Blish_HUD;
+using Blish_HUD.Input;
+using Blish_HUD.Modules;
+using Blish_HUD.Modules.Managers;
+using Blish_HUD.Settings;
+using Microsoft.Xna.Framework;
+using System;
+using System.ComponentModel.Composition;
+
+namespace Ideka.HitboxView
+{
+    [Export(typeof(Module))]
+    public class HitboxModule : Module
+    {
+        //private static readonly Logger Logger = Logger.GetLogger<HitboxModule>();
+
+        private static readonly (int min, int max) PingRange = (0, 1000);
+
+        internal static SettingsManager SettingsManager => Instance.ModuleParameters.SettingsManager;
+        internal static ContentsManager ContentsManager => Instance.ModuleParameters.ContentsManager;
+        internal static DirectoriesManager DirectoriesManager => Instance.ModuleParameters.DirectoriesManager;
+        internal static Gw2ApiManager Gw2ApiManager => Instance.ModuleParameters.Gw2ApiManager;
+
+        private static HitboxModule Instance { get; set; }
+
+        private HitboxEntity _hitbox;
+
+        private SettingEntry<Color> _hitboxColor;
+        private SettingEntry<KeyBinding> _toggleHitboxKey;
+        private SettingEntry<bool> _hitboxVisible;
+        private SettingEntry<bool> _hitboxSmoothing;
+        private SettingEntry<string> _gamePingString;
+        private SettingEntry<int> _gamePing;
+
+        [ImportingConstructor]
+        public HitboxModule([Import("ModuleParameters")] ModuleParameters moduleParameters) : base(moduleParameters)
+        {
+            Instance = this;
+        }
+
+        protected override void DefineSettings(SettingCollection settings)
+        {
+            // This setting is currently not visible as BlishHUD doesn't have a default control for Color settings
+            _hitboxColor = settings.DefineSetting("HitboxColor", Color.White,
+                () => Strings.SettingHitboxColor,
+                () => Strings.SettingHitboxColorText);
+            _toggleHitboxKey = settings.DefineSetting("ToggleHitboxKey", new KeyBinding(),
+                () => Strings.SettingToggleHitboxKey,
+                () => Strings.SettingToggleHitboxKeyText);
+            _hitboxVisible = settings.DefineSetting("HitboxVisible", true,
+                () => Strings.SettingHitboxVisible,
+                () => Strings.SettingHitboxVisibleText);
+            _hitboxSmoothing = settings.DefineSetting("HitboxSmoothing", true,
+                () => Strings.SettingHitboxSmoothing,
+                () => Strings.SettingHitboxSmoothingText);
+            _gamePingString = settings.DefineSetting("GamePingString", "100",
+                () => Strings.SettingGamePing,
+                () => Strings.SettingGamePingText);
+            _gamePing = settings.DefineSetting("GamePing", 100,
+                () => " ",
+                () => Strings.SettingGamePingText);
+            _gamePing.SetRange(PingRange.min, PingRange.max);
+
+            _gamePingString.SetValidation(value
+                => new SettingValidationResult(
+                    ParsePingString(value, out var ping) && $"{ping}" == value.Trim(),
+                    string.Format(Strings.SettingGamePingValidation, PingRange.min, PingRange.max)));
+        }
+
+        protected override void Initialize()
+        {
+            _hitbox = new HitboxEntity()
+            {
+                Smoothing = _hitboxSmoothing.Value,
+                Ping = _gamePing.Value,
+            };
+
+            if (_hitboxVisible.Value)
+                GameService.Graphics.World.AddEntity(_hitbox);
+
+            _hitboxColor.SettingChanged += HitboxColorChanged;
+            _toggleHitboxKey.Value.Enabled = true;
+            _toggleHitboxKey.Value.Activated += HitboxToggled;
+            _hitboxVisible.SettingChanged += HitboxVisibleChanged;
+            _hitboxSmoothing.SettingChanged += HitboxSmoothingChanged;
+            _gamePingString.SettingChanged += GamePingStringChanged;
+            _gamePing.SettingChanged += GamePingChanged;
+        }
+
+        private void HitboxColorChanged(object sender, ValueChangedEventArgs<Color> e)
+            => _hitbox.Color = _hitboxColor.Value;
+
+        private void HitboxToggled(object sender, EventArgs e)
+            => _hitboxVisible.Value = !_hitboxVisible.Value;
+
+        private void HitboxVisibleChanged(object sender, ValueChangedEventArgs<bool> e)
+        {
+            _hitbox.Reset();
+            GameService.Graphics.World.RemoveEntity(_hitbox);
+            if (_hitboxVisible.Value)
+                GameService.Graphics.World.AddEntity(_hitbox);
+        }
+
+        private void HitboxSmoothingChanged(object sender, ValueChangedEventArgs<bool> e)
+            => _hitbox.Smoothing = _hitboxSmoothing.Value;
+
+        private bool _reflecting = false;
+        private void GamePingStringChanged(object sender, ValueChangedEventArgs<string> e)
+        {
+            if (_reflecting)
+                return;
+
+            _reflecting = true;
+            _gamePing.Value = ParsePingString(_gamePingString.Value, out int ping) ? ping : _gamePing.Value;
+            _reflecting = false;
+
+            _hitbox.Ping = _gamePing.Value;
+        }
+
+        private void GamePingChanged(object sender, ValueChangedEventArgs<int> e)
+        {
+            if (_reflecting)
+                return;
+
+            _reflecting = true;
+            _gamePingString.Value = $"{_gamePing.Value}";
+            _reflecting = false;
+
+            _hitbox.Ping = _gamePing.Value;
+        }
+
+        protected override void Unload()
+        {
+            _hitboxColor.SettingChanged -= HitboxColorChanged;
+            _toggleHitboxKey.Value.Activated -= HitboxToggled;
+            _hitboxVisible.SettingChanged -= HitboxVisibleChanged;
+            _hitboxSmoothing.SettingChanged -= HitboxSmoothingChanged;
+            _gamePingString.SettingChanged -= GamePingStringChanged;
+            _gamePing.SettingChanged -= GamePingChanged;
+
+            GameService.Graphics.World.RemoveEntity(_hitbox);
+            _hitbox?.Dispose();
+
+            Instance = null;
+        }
+
+        private static bool ParsePingString(string pingString, out int ping)
+        {
+            var parsed = int.TryParse(pingString, out ping);
+            ping = Math.Min(Math.Max(ping, PingRange.min), PingRange.max);
+            return parsed;
+        }
+    }
+}
---
+++ HitboxView/World/HitboxEntity.cs
@@ -0,0 +1,188 @@
+﻿using Blish_HUD;
+using Blish_HUD.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ideka.HitboxView
+{
+    public class HitboxEntity : IEntity, IDisposable
+    {
+        private static readonly TimeSpan SmoothingCompensation = TimeSpan.FromMilliseconds(100 / 6);
+
+        public float DrawOrder => 100;  // TODO: Figure out what to set this to.
+
+        public bool Smoothing { get; set; } = true;
+
+        private Color _color = Color.White;
+        public Color Color
+        {
+            get => _color;
+            set
+            {
+                _color = value;
+                if (_effect != null)
+                    _effect.DiffuseColor = _color.ToVector3();
+            }
+        }
+
+        private TimeSpan _delay;
+        public TimeSpan Delay
+        {
+            get => _delay + (Smoothing ? SmoothingCompensation : TimeSpan.Zero);
+            set => _delay = value;
+        }
+
+        public int Ping
+        {
+            get => (int)Delay.TotalMilliseconds;
+            set => Delay = TimeSpan.FromMilliseconds(value);
+        }
+
+        public Vector3 Position { get; private set; }
+        public Vector3 Forward { get; private set; }
+        public bool IsDisposed { get; private set; }
+
+        private static Vector3 PlayerPosition => GameService.Gw2Mumble.RawClient.AvatarPosition.ToXnaVector3();
+        private static Vector3 PlayerForward => GameService.Gw2Mumble.RawClient.AvatarFront.ToXnaVector3();
+
+        private class TimePos
+        {
+            public readonly TimeSpan Time;
+            public readonly Vector3 Position;
+            public readonly Vector3 Forward;
+
+            public TimePos(TimeSpan time, Vector3 position, Vector3 forward)
+            {
+                Time = time;
+                Position = position;
+                Forward = forward;
+            }
+
+            public TimePos(Vector3 position, Vector3 forward) : this(default, position, forward)
+            {
+            }
+
+            public TimePos(TimeSpan time) : this(time, PlayerPosition, PlayerForward)
+            {
+            }
+
+            public static bool AreEquivalent(TimePos a, TimePos b)
+                => a.Position == b.Position && a.Forward == b.Forward;
+
+            public static TimePos Lerp(TimePos a, TimePos b, TimeSpan delay, TimeSpan time)
+            {
+                float p;
+                {
+                    var start = (a.Time + delay).TotalMilliseconds;
+                    var now = time.TotalMilliseconds;
+                    var end = (b.Time + delay).TotalMilliseconds;
+                    p = (float)((now - start) / (end - start));
+                }
+
+                return float.IsNaN(p) || float.IsInfinity(p)
+                    ? a
+                    : new TimePos(
+                        Vector3.Lerp(a.Position, b.Position, p),
+                        Vector3.Lerp(a.Forward, b.Forward, p));
+            }
+        }
+
+        private readonly Quad _quad;
+        private readonly Texture2D _texture;
+        private BasicEffect _effect;
+
+        private int _lastTick;
+        private TimePos _lastPopped; 
+        private TimePos _lastQueued; 
+        private readonly Queue<TimePos> _timePosQueue = new Queue<TimePos>();
+
+        public HitboxEntity()
+        {
+            _quad = new Quad(Vector3.Zero, Vector3.Backward, Vector3.Up, 1, 1);
+            _texture = HitboxModule.ContentsManager.GetTexture("Hitbox.png");
+            GameService.Graphics.QueueMainThreadRender(graphicsDevice =>
+            {
+                if (_effect != null || IsDisposed)
+                    return;
+
+                _effect = new BasicEffect(graphicsDevice)
+                {
+                    Texture = _texture,
+                    TextureEnabled = true,
+                    DiffuseColor = Color.ToVector3(),
+                };
+            });
+
+            Reset();
+        }
+
+        public void Reset()
+        {
+            _lastQueued = null;
+            _lastPopped = new TimePos(TimeSpan.Zero);
+            _timePosQueue.Clear();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (GameService.Gw2Mumble.RawClient.Tick > _lastTick)
+            {
+                _lastTick = GameService.Gw2Mumble.RawClient.Tick;
+
+                var newTimePos = new TimePos(gameTime.TotalGameTime);
+
+                // Don't enqueue the same position multiple times in a row
+                if (_lastQueued == null || !TimePos.AreEquivalent(_lastQueued, newTimePos))
+                {
+                    _lastQueued = new TimePos(gameTime.TotalGameTime);
+                    _timePosQueue.Enqueue(_lastQueued);
+                }
+            }
+
+            while (_timePosQueue.Any() && _timePosQueue.Peek().Time + Delay <= gameTime.TotalGameTime)
+                _lastPopped = _timePosQueue.Dequeue();
+
+            void apply(TimePos timePos)
+            {
+                Position = timePos.Position;
+                Forward = timePos.Forward;
+            }
+
+            if (!Smoothing || !_timePosQueue.Any())
+                apply(_lastPopped);
+            else
+                apply(TimePos.Lerp(_lastPopped, _timePosQueue.Peek(), Delay, gameTime.TotalGameTime));
+        }
+
+        public void Render(GraphicsDevice graphicsDevice, IWorld world, ICamera camera)
+        {
+            if (_effect == null)
+                return;
+
+            _effect.View = GameService.Gw2Mumble.PlayerCamera.View;
+            _effect.Projection = GameService.Gw2Mumble.PlayerCamera.Projection;
+
+            var worldMatrix = Matrix.CreateScale(1, 1, 1) * Matrix.CreateTranslation(Position);
+            var t = worldMatrix.Translation;
+            worldMatrix *= Matrix.CreateRotationZ(-(float)Math.Atan2(Forward.X, Forward.Y));
+            worldMatrix.Translation = t;
+            _effect.World = worldMatrix;
+
+            foreach (EffectPass pass in _effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                graphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, _quad.Vertices, 0, 4, _quad.Indexes, 0, 2);
+            }
+        }
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+            _texture?.Dispose();
+            _effect?.Dispose();
+        }
+    }
+}
